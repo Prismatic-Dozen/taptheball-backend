16950c9b9dfe91730d6114a678411b3b42eb6d9b
Prismatic-Dozen <mymailsendservice@gmail.com>
2026-02-25 17:10:26 +0530

    error for auth route fixed

--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { googleAuth } = require("./auth.controller");
+const { playGamesAuth } = require("./auth.controller");
 
 router.post("/auth/playgames", playGamesAuth);
 
